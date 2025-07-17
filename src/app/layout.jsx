78f2c47ee7f69cdcfc183a1043c51c821c2cc7d6
Shashwat Srivastava <shashwat0805@protonmail.com>
2025-07-17 22:20:18 +0530

    Refactor layout structure; introduce LayoutWrapper component to conditionally render header and footer

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,5 @@
+import LayoutWrapper from "@/components/LayoutWrapper";
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 
 export const metadata = {
   title: "PESU Library System",
@@ -11,9 +10,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="hydrated">
       <body className={`flex flex-col min-h-screen`} data-new-gr-c-s-check-loaded="14.1244.0" data-gr-ext-installed="">
-        <Header />
-        <main className="flex-1 px-6 py-4 bg-gray-50">{children}</main>
-        <Footer />
+        <LayoutWrapper>
+          {children}
+        </LayoutWrapper>
       </body>
     </html>
   );
